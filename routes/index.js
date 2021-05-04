67671afed195c92dab56a788de854518be6746f2
vidhi-mody <vidhimody6@gmail.com>
2021-05-05 00:19:04 +0530

    edit: heading

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   const users = await User.find({ completed: true });
 
-  res.render('index', { title: 'Questionnaire', users });
+  res.render('index', { title: 'Responses', users });
 });
 
 router.get('/:userId', async (req, res, next) => {
   const user = await User.findOne({ userId: req.params.userId }).lean();
 
-  res.render('user', { title: 'Questionnaire', user });
+  res.render('user', { title: 'Student Report', user });
 });
 
 module.exports = router;
